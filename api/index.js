fb5ab768f0cd79ded50aa566c355ad427eed9664
Saksham Singh <developer.sakshamsingh@gmail.com>
2024-01-13 12:29:11 +0530

    database setup

--- api/index.js
+++ api/index.js
@@ -1,14 +1,24 @@
 import express from 'express'
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
-const PORT = 3000;
+
+mongoose.connect(process.env.DB_CONNECTION) // Db Connection
+.then(()=>{
+    console.log("DATABASE Conncted....");
+})
+.catch((err)=>{
+    console.log("DATABASE Connection Error "+err);
+});
+
+app.listen(process.env.PORT, ()=>{ // Server Up
+    console.log(`Server is running on Port : ${process.env.PORT}`);
+} )
 
 app.get('/', (req,res)=>{
     res.status(200).json({
-        "msg":"welcome buddy"
+        "msg":"welcome buddy" 
     })
 })
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running on Port : ${PORT}`);
-} )
